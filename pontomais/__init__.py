75dad4fe090cbe367cd77b9747c43d717e94a5b8
Lucas Coppio <lucascoppio@gmail.com>
2020-04-23 09:55:00 -0300

    generates a hidden folder and put the credentials there

--- pontomais/__init__.py
+++ pontomais/__init__.py
@@ -1,4 +1,13 @@
+import os
+from pathlib import Path
+
 __version__ = "0.0.1"
 API_ROOT = 'https://api.pontomais.com.br/api'
-CREDENTIAL_FILE = ".creds"
-PROFILE_FILE = ".profile"
+
+pontomais_dir = os.path.join(str(Path.home()), ".pontomais")
+
+if not os.path.exists(pontomais_dir):
+    os.makedirs(pontomais_dir)
+
+CREDENTIAL_FILE =  os.path.join(pontomais_dir, ".creds")
+PROFILE_FILE = os.path.join(pontomais_dir, ".profile")
